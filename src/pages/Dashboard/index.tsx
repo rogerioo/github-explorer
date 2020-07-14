79a1924fdf7d63e6391de6ffb88f9fe19d4744a4
Rogério Júnior <junior.rogerio8@gmail.com>
2020-07-14 03:43:27 -0300

    Add stylized dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,7 +1,56 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => <Title>Explore repositórios no Github</Title>;
+import { Title, Form, Repositories } from './styles';
+
+const Dashboard: React.FC = () => (
+  <>
+    <img src={logoImg} alt="github Explorer" />
+    <Title>Explore repositórios no Github</Title>
+
+    <Form>
+      <input placeholder="Digite o nome do repositório" />
+      <button type="submit">Pesquisar</button>
+    </Form>
+
+    <Repositories>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/42387946?s=460&u=e96bfdeff7e1fdfa8985698791bd37dd5ef82af6&v=4"
+          alt="Rogério Júnior"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Esta é uma descrição top</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/42387946?s=460&u=e96bfdeff7e1fdfa8985698791bd37dd5ef82af6&v=4"
+          alt="Rogério Júnior"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Esta é uma descrição top</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+      <a href="teste">
+        <img
+          src="https://avatars1.githubusercontent.com/u/42387946?s=460&u=e96bfdeff7e1fdfa8985698791bd37dd5ef82af6&v=4"
+          alt="Rogério Júnior"
+        />
+        <div>
+          <strong>rocketseat/unform</strong>
+          <p>Esta é uma descrição top</p>
+        </div>
+        <FiChevronRight size={20} />
+      </a>
+    </Repositories>
+  </>
+);
 
 export default Dashboard;
